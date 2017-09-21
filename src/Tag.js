3a3aecf2f50910173e8d36cfaeffb3beead9effa
kaimiyang <28302551@qq.com>
2017-09-21 11:15:31 +0800

    Test css file

--- src/Tag.js
+++ src/Tag.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './style.css'
-
 export default class Tag extends React.Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
