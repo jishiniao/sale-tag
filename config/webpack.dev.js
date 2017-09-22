54b7eb750b7a122b19e9ecc9984b653e525e4eaa
kaimiyang <28302551@qq.com>
2017-09-22 16:56:46 +0800

    Change config.file

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -47,15 +47,15 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'env',
-              "stage-0"
-            ]
-          }
+          loader: 'babel-loader'
+          // options: { //.babelrc
+          //   presets: [
+          //     'env',
+          //     "stage-0"
+          //   ]
+          // }
         }
       }
     ]
